7fdc3b454eb1c3c83b03005e4339e6b39ae883a8
Yinghao Zhu <yhzhu99@gmail.com>
2022-02-06 21:36:25 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
 sudo apt-get install libmysqlclient-dev
 pip install mysqlclient -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install fastapi[all] -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install sqlalchemy -i https://pypi.tuna.tsinghua.edu.cn/simple
