c54a572e730eab4b17a401e4c02dcbf1a27b28be
Yinghao Zhu <yhzhu99@gmail.com>
2022-02-10 22:45:41 +0800

    chore: no longer use 404 status code

--- main.py
+++ main.py
@@ -60,25 +60,25 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=250, detail="User not found")
     return db_user
 
 @app.post("/users/login", response_model=schemas.User)
 def login(username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=username)
     if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=250, detail="User not found")
     if db_user.password != password:
-        raise HTTPException(status_code=250, detail="Password not correct")
+        raise HTTPException(status_code=251, detail="Password not correct")
     return db_user
 
 @app.post("/users/register", response_model=schemas.UserCreate)
 def register(username: str = Form(...), password1: str = Form(...),password2: str = Form(...), db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=username)
     if db_user is not None:
-        raise HTTPException(status_code=404, detail="User already registered")
+        raise HTTPException(status_code=250, detail="User already registered")
     if password1 != password2:
-        raise HTTPException(status_code=250, detail="密码不同")
+        raise HTTPException(status_code=251, detail="密码不同")
     new_user = schemas.UserCreate(username=username, password=password1, usertype=0)
     crud.create_user(db,new_user)
     return new_user
