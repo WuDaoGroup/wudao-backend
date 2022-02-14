c9307e97f9902859a289b86e61bacc57f146f509
Yinghao Zhu <yhzhu99@gmail.com>
2022-02-14 16:22:54 +0800

    chore: switch to postgresql as database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://buaase2021:buaase(2021)@rm-uf6ji600qianqe6921o.mysql.rds.aliyuncs.com:3306/cloud"
+SQLALCHEMY_DATABASE_URL = "postgresql://pujsuqaoqnrzyz:8e977decc5189564c1b10562041b0de3062e91aee8ca316d12c800809b5a6b82@ec2-35-175-68-90.compute-1.amazonaws.com:5432/ddd9r0vs09pdtg"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
