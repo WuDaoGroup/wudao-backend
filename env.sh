6b91d347b7ccc931cef4c663ea241148fd76feeb
Yinghao Zhu <yhzhu99@gmail.com>
2022-02-14 15:34:01 +0800

    ci: test vercel

--- env.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install libmysqlclient-dev
-pip install mysqlclient -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install fastapi[all] -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install sqlalchemy -i https://pypi.tuna.tsinghua.edu.cn/simple
