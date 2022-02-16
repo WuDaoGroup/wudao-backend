6c245d1f29df1bb80e4a1d443428f96ae9b4b19e
echo17666 <3239695085@qq.com>
2022-02-16 22:18:26 +0800

    refactor: code style

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from pandas.api.types import CategoricalDtype
-from io import StringIO
 
-import crud, models, schemas
+import crud, models, schemas,os,csv
 import pandas as pd
 import numpy as np
 from database import SessionLocal, engine
@@ -94,11 +93,18 @@ def register(username: str = Form(...), password1: str = Form(...),password2: st
 
 @app.post("/files/upload")
 async def create_upload_file(upload_file: UploadFile = File(...)):
+    file_type=os.path.splitext(upload_file.filename)[1]
+    if file_type not in [".csv", ".xls", ".xlsx"]:
+        raise HTTPException(status_code=240, detail="File type not correct")
     file_location = f"./data/{upload_file.filename}"
     print('get_file:',file_location)
     with open(file_location, "wb+") as file_object:
         file_object.write(upload_file.file.read())
+    data = pd.read_csv(f"./data/{upload_file.filename}",engine = "python",header=None)
+    df = pd.DataFrame(data)
+    print(df)
     return {"info": f"file '{upload_file.filename}' saved at '{file_location}'"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8123, reload=True)
