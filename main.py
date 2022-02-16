e5f106149ee852e7699f027d3e31a1cbde17a7b3
DHUAVY <3484432441@qq.com>
2022-02-16 21:44:55 +0800

    feat: init model dev api (predict using boost tree)

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import uvicorn
-from fastapi import Depends, FastAPI, HTTPException, Request, Response, Form
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from fastapi import Depends, FastAPI, HTTPException, Request, Response, Form, File, UploadFile
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostRegressor
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
@@ -82,6 +88,52 @@ def register(username: str = Form(...), password1: str = Form(...),password2: st
     new_user = schemas.UserCreate(username=username, password=password1, usertype=0)
     crud.create_user(db,new_user)
     return new_user
+
+@app.post("/predict/ols")
+def ordinary_least_squares(filename: str = Form(...)):
+    res = {}
+    data = pd.read_csv('./data/'+ filename)
+    reg = linear_model.LinearRegression()
+    X = data.iloc[:, 1:]
+    y = data.iloc[:, :1]
+    reg.fit( X, y )
+    reg_list1 = reg.coef_.tolist()
+    reg_list2 = reg.intercept_.tolist()
+    res["result_coef"] = reg_list1[0]
+    res["result_intercept"] = reg_list2[0]
+    return res
+
+@app.post("/predict/bdtr")
+def boosted_decision_tree_regression( filename: str = Form(...), id: str = Form(...)):
+    the_addr = 'C:\\Users\\DELL\\Desktop\\wudao-backend\\pictures\\' + filename + id + '_handle_result.png'
+    res = {}
+    data = pd.read_csv('./data/'+ filename) 
+    rng = np.random.RandomState(1)
+    X = data.iloc[:, 1:2]
+    y = data.iloc[:, 0:1]
+    regr_1 = DecisionTreeRegressor(max_depth=4)
+    regr_2 = AdaBoostRegressor(
+        DecisionTreeRegressor(max_depth=4), n_estimators=300, random_state=rng
+    )
+    regr_1.fit(X, y)
+    regr_2.fit(X, y)
+    y_1 = regr_1.predict(X)
+    y_2 = regr_2.predict(X)
+    plt.figure()
+    plt.scatter(X, y, c="k", label="training samples")
+    plt.plot(X, y_1, c="g", label="n_estimators=1", linewidth=2)
+    plt.plot(X, y_2, c="r", label="n_estimators=300", linewidth=2)
+    plt.xlabel("data")
+    plt.ylabel("target")
+    plt.title("Boosted Decision Tree Regression")
+    plt.legend()
+    plt.savefig(the_addr)
+    
+    res['pic_addr'] = the_addr
+    return res
+
+
+
 
 
 if __name__ == "__main__":
