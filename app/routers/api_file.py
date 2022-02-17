17337474d6c893f1825c91e0548c2bb157a1055b
echo17666 <3239695085@qq.com>
2022-02-17 20:32:38 +0800

    chore: delete download api

--- app/routers/api_file.py
+++ app/routers/api_file.py
@@ -10,20 +10,13 @@ import app.crud as crud
 from app.database import SessionLocal
 
 router = APIRouter(prefix = "/files")
-@router.get("/upload")
+@router.post("/upload")
 async def upload_file(upload_file: UploadFile = File(...)):
     file_type=os.path.splitext(upload_file.filename)[1]
     if file_type not in [".csv", ".xls", ".xlsx"]:
         raise HTTPException(status_code=240, detail="File type not correct")
-    file_location = f"./data/{upload_file.filename}"
-    print('get_file:',file_location)
+    file_location = f"./static/data/{upload_file.filename}"
+    print(file_location)
     with open(file_location, "wb+") as file_object:
         file_object.write(upload_file.file.read())
-    data = pd.read_csv(f"./data/{upload_file.filename}",engine = "python",header=None)
-    df = pd.DataFrame(data)
-    print(df)
     return {"info": f"file '{upload_file.filename}' saved at '{file_location}'"}
-
-@router.get("/download/{file_addr}")
-def get_image(file_addr: str):
-    return FileResponse(path=file_addr, filename=file_addr)
