708b3b5572e481e728565460ebb65dfcf56a35c7
echo17666 <3239695085@qq.com>
2022-02-23 16:00:38 +0800

    feat(api):  filter data by checking zscore

--- app/routers/api_data.py
+++ app/routers/api_data.py
@@ -46,3 +46,76 @@ async def return_data_basic_file_info(data_filename: str):
         'header':header
     }
     return response
+
+
+
+
+@router.get("/{data_filename}_selected_feature.csv/zscore")
+async def features_zscore(data_filename: str):
+    df = pd.read_csv(f"./static/data/{data_filename}_selected_feature.csv")
+    df_score = df.copy()
+    df_score = (df_score-df_score.mean())/(df_score.std()+1e-12)
+    df_score.to_csv(f'./static/data/{data_filename}_zscore.csv', index=False)
+    res = df_score.to_json(orient="records")
+    parsed = json.loads(res)
+    response={
+        'content': parsed,
+    }
+    return response
+
+
+@router.get("/{data_filename}_selected_feature.csv/zscore/mean")
+async def zscore_zero(data_filename: str):
+    df = pd.read_csv(f"./static/data/{data_filename}_zscore.csv")
+    df_score = df.copy()
+    df_score.fillna(value = 0,inplace=True)
+    df_score.to_csv(f'./static/data/{data_filename}_zscore_fill.csv', index=False)
+    res = df_score.to_json(orient="records")
+    parsed = json.loads(res)
+    response={
+        'content': parsed,
+    }
+    return response
+
+@router.get("/{data_filename}_selected_feature.csv/zscore/median")
+async def zscore_median(data_filename: str):
+    df = pd.read_csv(f"./static/data/{data_filename}_selected_feature.csv")
+    df_score = pd.read_csv(f"./static/data/{data_filename}_zscore.csv")
+    h = []
+    for idx, e in enumerate(df.columns):
+        s={}
+        origin_mean = float(df[e].mean())
+        origin_std = float(df[e].std())
+        origin_median = float(df[e].median())
+        fill_median = (origin_median - origin_mean)/(origin_std+1e-12)
+        s['a']= fill_median
+        h.append(s)
+    i = 0
+    for idx, e in enumerate(df_score.columns):
+        df_score[e].fillna(value = float(h[idx]['a']),inplace=True)
+        print (idx)
+    
+    df_score.to_csv(f'./static/data/{data_filename}_zscore_fill.csv', index=False)
+    res = df_score.to_json(orient="records")
+    parsed = json.loads(res)
+    response={
+        'content': parsed,
+    }
+    return response
+
+
+@router.get("/{data_filename}_selected_feature.csv/zscore/filter")
+async def features_filter(data_filename: str, bar: float):
+    df = pd.read_csv(f"./static/data/{data_filename}_zscore_fill.csv")
+    for f in df.columns:
+        # print("before ---------------------------------")
+        # print()
+        df = df[abs(df[f])<bar]
+        print (f,df,"bb////////////////////////////")
+    df.to_csv(f'./static/data/{data_filename}_zscore.csv', index=False)
+    res = df.to_json(orient="records")
+    parsed = json.loads(res)
+    response={
+        'content': parsed,
+    }
+    return response
