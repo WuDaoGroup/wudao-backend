c33a0afbc1f361c1a9cc2acef3389efb370b80de
Yinghao Zhu <yhzhu99@gmail.com>
2022-03-22 18:07:00 +0800

    feat: data reduction pca/tsne

--- app/routers/api_explanation.py
+++ app/routers/api_explanation.py
@@ -1,4 +1,5 @@
 import os,csv,json
+import pathlib
 
 from fastapi import APIRouter, Depends, FastAPI, HTTPException, Request, Response, Form, File, UploadFile
 from fastapi.responses import FileResponse
@@ -23,29 +24,46 @@ from app.database import SessionLocal
 
 router = APIRouter(prefix = "/explanation")
 
-@router.post("/{data_filename}/dimension_reduction")
-async def return_dimension_reduction(info: schemas.ExplanationInfo,data_filename: str):
-    df = pd.read_csv(f"./static/data/{data_filename}_zscore_fill_filter.csv")
-    data_array = np.array(df)
-    if info.type=='PCA': # 判断降维类别
-        reduction_model = PCA().fit_transform(data_array)
-    elif info.type=='TSNE':
-        reduction_model = TSNE(n_components=int(info.dimmension), learning_rate=int(info.learningrate)).fit_transform(data_array)
-    target=df[info.target].tolist()
-    if info.dimmension=='2': # 判断降维维度
-        plt.figure(figsize=(15, 8))
-        plt.subplot(121)
-        plt.scatter(reduction_model[:, 0], reduction_model[:, 1], c = target)
-    elif info.dimmension=='3':
-        fig = plt.figure()
+@router.post("/reduction")
+async def data_dimension_reduction(username: str = Form(...), method: str = Form(...), dimension: int = Form(...), target: str = Form(...)):
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 中文字体设置-黑体
+    plt.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
+    df = pd.read_csv(f'./static/data/{username}/data_zscore_fill_filter.csv')
+    if method=='PCA': # 判断降维类别
+        reduction_model = PCA().fit_transform(df)
+    elif method=='TSNE':
+        reduction_model = TSNE(n_components=dimension, learning_rate='auto').fit_transform(df)
+    df_target=df[target]
+    if dimension ==2: # 判断降维维度
+        df_subset = pd.DataFrame({'2d-one': reduction_model[:,0], '2d-two': reduction_model[:,1], 'target': df_target})
+        plt.figure(figsize=(10,10))
+        sns.scatterplot(
+            x="2d-one", y="2d-two",
+            hue="target",
+            palette=sns.color_palette("hls", df_target.shape[0]),
+            data=df_subset,
+            legend=False,
+            alpha=0.3,
+        )
+    elif dimension==3:
+        fig = plt.figure(figsize=(10,10))
         ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(reduction_model[:, 0], reduction_model[:, 1],reduction_model[:, 2], c=target)
-    plt.savefig(f'./static/images/{data_filename}_dimension_reduction_img.png') # 存储图片
-
-    response={
-        'pic_addr': './static/images/{data_filename}_dimension_reduction_img.png'
-    }
-    return response
+        df_subset = pd.DataFrame({'3d-one': reduction_model[:,0], '3d-two': reduction_model[:,1], '3d-three': reduction_model[:,2], 'target': df_target})
+        ax.scatter(
+            xs=df_subset["3d-one"], 
+            ys=df_subset["3d-two"], 
+            zs=df_subset["3d-three"], 
+            c=df_subset["target"], 
+            cmap='tab10'
+        )
+        ax.set_xlabel('3d-one')
+        ax.set_ylabel('3d-two')
+        ax.set_zlabel('3d-three')
+    plt.title(f'Reduction Result of {method} with {dimension} dimensions', fontsize=18)
+    pathlib.Path(f'./static/data/{username}/images/explanation').mkdir(parents=True, exist_ok=True)
+    plt.savefig(f'./static/data/{username}/images/explanation/reduction_{method}_{dimension}_{target}.png')
+    res = {'message': 'reduction success'}
+    return res
 
 @router.post("/{data_filename}/feature_corr")
 async def return_feature_corr(data_filename: str, methods: str):
