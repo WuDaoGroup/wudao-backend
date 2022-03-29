7382da15ffbff836691e095130258aad8810ed39
Yinghao Zhu <yhzhu99@gmail.com>
2022-03-29 17:22:18 +0800

    chore: update causal related filename

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,4 +1,4 @@
-from app.routers import api_user, api_file, api_data, api_model, api_explanation, api_causals
+from app.routers import api_causal, api_user, api_file, api_data, api_model, api_explanation
 
 
 prefix = '/api/v1'
@@ -8,4 +8,4 @@ def init_routers(app):
     app.include_router(api_data.router, prefix=prefix, tags=["数据处理"])
     app.include_router(api_model.router, prefix=prefix, tags=["模型相关"])
     app.include_router(api_explanation.router, prefix=prefix, tags=["可解释性"])
-    app.include_router(api_causals.router, prefix=prefix, tags=["因果推断"])
+    app.include_router(api_causal.router, prefix=prefix, tags=["因果推断"])
